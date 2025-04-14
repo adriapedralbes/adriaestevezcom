24fa24752c20604a09a9c749c9ac1ad08e5d5788
adriapedralbes <a23adrestmar@inspedralbes.cat>
2025-04-14 19:20:55 +0200

    feat: actualizar supabaseClient para usar variables de entorno en lugar de valores codificados

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Use environment variables in a real application
-const supabaseUrl = 'https://tamfudvevdvygetqmttr.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhbWZ1ZHZldmR2eWdldHFtdHRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NDc5OTgsImV4cCI6MjA2MDIyMzk5OH0.scBT7k3Z6MG-x5_JiTgxytyNNnNwAfcNggG6CQMK8D4'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 // It's better practice to store these in environment variables (.env.local)
 // const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
