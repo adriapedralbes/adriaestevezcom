b851a0684403ed6e8d49c5bfdd223a20f451c2c1
adriestevezdev <adria.esmarquez@gmail.com>
2025-05-16 09:08:02 +0200

    feat: actualizar logo en el componente Logo para reflejar la nueva identidad visual

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,20 +11,20 @@ interface LogoProps {
 
 export function Logo({ width = 96, height = 96, className = "" }: LogoProps) {
   const [fallbackToImg, setFallbackToImg] = useState(false);
-  
+
   // If the image fails to load with Next.js Image, fall back to regular img tag
   return fallbackToImg ? (
-    <img 
-      src="/logo_futurprive_sinfondo.png" 
-      alt="FuturPrive Logo" 
+    <img
+      src="/logo_arquitectosIA.png"
+      alt="ArquitectosIA Logo"
       width={width}
       height={height}
       className={className}
     />
   ) : (
-    <Image 
-      src="/logo_futurprive_sinfondo.png" 
-      alt="FuturPrive Logo" 
+    <Image
+      src="/logo_arquitectosIA.png"
+      alt="ArquitectosIA Logo"
       width={width}
       height={height}
       className={className}
